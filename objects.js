0e90321c2d0f46df6bd92ddebbea4f8327c4e147
Robert Lego <robertlego@gmail.com>
2018-01-27 22:46:17 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,10 +4,9 @@ var playlist = {
 };
 
 function updatePlaylist(playlist, artist, song) {
-  playlist['Phil Ochs']:"Here's to the State of Mississippi";
+  playlist['Phil Ochs'] = "Here's to the State of Mississippi";
 }
 
 function removeFromPlaylist(playlist, artist) {
   delete playlist[artist];
 }
-
