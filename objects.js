cbb52561472c1f656d4dc1a920adde1ca0923579
Robert Lego <robertlego@gmail.com>
2018-01-27 22:38:03 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,12 @@
+var playlist = {
+  Tom Petty:'Runnin Down a Dream',
+  Eagles:'Hotel California'
+  Def Leppard:'Animal'};
+
+function updatePlaylist(playlist, artist, song){
+  playlist.Scorpions = 'Wind of Change';
+}
+
+function removeFromPlaylist(playlist, artist){
+  Delete playlist[artist];
+}
